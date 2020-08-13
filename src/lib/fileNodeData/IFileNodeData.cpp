37bab90f2289faace38ac99a37fee9e0e4970cb4
Sebastian Engel <dev@sebastianengel.eu>
2020-08-14 00:17:39 +0200

    WIP add FileNodeData

--- src/lib/fileNodeData/IFileNodeData.cpp
+++ src/lib/fileNodeData/IFileNodeData.cpp
@@ -6,11 +6,12 @@ IFileNodeData::IFileNodeData() {}
 
 IFileNodeData::~IFileNodeData() {}
 
-librevenge::RVNGInputStream &operator>>(librevenge::RVNGInputStream &input, IFileNodeData &obj)
+const libone::RVNGInputStreamPtr_t &operator>>(const libone::RVNGInputStreamPtr_t &input, IFileNodeData &obj)
 {
   obj.parse(input);
   return input;
 }
 
 } // namespace libone
+
 /* vim:set shiftwidth=2 softtabstop=2 expandtab: */
